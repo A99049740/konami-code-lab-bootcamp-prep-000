7ac44cb71b182b6bbc0fd0184e25c49b9059a6f2
jill zhang <talktojillzhang@gmail.com>
2018-09-02 01:08:41 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,24 @@ const codes = [
   "a"
 ];
 
+index =0;
 function init() {
   // your code here
+  document.addEventListener('click',function(e){
+    alert("sfds")
+  })
+}
+
+function check(e){
+  const key = e.key;
+  alert(key)
+  if (key === codes[index]){
+    index++
+    if (index === codes.length){
+      alert()
+      index =0
+    }
+  } else{
+    index =0 
+  }
 }
